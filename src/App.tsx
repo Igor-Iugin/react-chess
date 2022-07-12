c7a3dcad96ac1eb8b1660b2adf4d302bdef49c07
Igor Iugin <gigorsaurus@gmail.com>
2022-07-12 22:52:23 +0300

    Styled App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import classNames from 'classnames/bind'
+
+import { Board } from './components/Board'
+import { BoardModel } from './models/BoardModel'
+
+import styles from './App.module.css'
+
+
+const cn = classNames.bind(styles)
 
 function App() {
-  return (
-     <></>
+   const [board, setBoard] = useState(new BoardModel())
+
+   useEffect(() => {
+      restart()
+   }, [])
+
+
+
+   function restart() {
+      const newBoard = new BoardModel()
+      newBoard.init()
+      setBoard(newBoard)
+   }
+
+   return (
+     <main className={cn('wrapper')}>
+        <Board board={board} setBoard={setBoard} />
+     </main>
   )
 }
 
